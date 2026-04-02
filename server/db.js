58c4cd55aed09474e59ddd01e84c1ca5a498344d
caushik1atwit <caushik1@wit.edu>
2026-04-01 23:55:29 -0400

    read env correctly

--- server/db.js
+++ server/db.js
@@ -3,9 +3,9 @@ const { Pool } = require('pg');
 const pool = new Pool({
   host:     process.env.DB_HOST     || 'localhost',
   port:     parseInt(process.env.DB_PORT || '5432'),
-  user:     process.env.POSTGRES_USER     || 'postgres',
-  password: process.env.POSTGRES_PASSWORD || 'postgres',
-  database: process.env.POSTGRES_DB       || 'shopping_cart',
+  user:     process.env.DB_USER     || process.env.POSTGRES_USER     || 'postgres',
+  password: process.env.DB_PASSWORD || process.env.POSTGRES_PASSWORD || 'postgres',
+  database: process.env.DB_NAME     || process.env.POSTGRES_DB       || 'shopping_cart',
 });
 
 module.exports = pool;
